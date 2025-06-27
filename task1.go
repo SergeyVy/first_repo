8a4238345bdcf51b6ac987de186224d696d444ba
Sergey Vydumkin <sergvydum@gmail.com>
2025-06-27 22:48:33 +0300

    решал заного task1

---
+++ task1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+	"math"
+)
+
+func secondMax(numbers []int) (int, error) {
+	if len(numbers) < 2 {
+		return 0, errors.New("в срезе элементов меньше двух")
+	}
+
+	max1 := math.MinInt
+	max2 := math.MinInt
+
+	for _, num := range numbers {
+		if num > max1 {
+			max2 = max1
+			max1 = num
+		} else if num > max2 && num != max1 {
+			max2 = num
+		}
+	}
+
+	if max2 == math.MinInt {
+		return 0, errors.New("в срезе нет второго уникального максимума")
+	}
+
+	return max2, nil
+}
+
+func main() {
+
+
+result, err := secondMax([]int{2, 5, 6, 8, 9})
+	if err != nil {
+		fmt.Println("Ошибка:", err)
+	} else {
+		fmt.Println("Второе максимальное число:", result)
+	}
+}
+	
--- task4.go
+++ task4.go
@@ -1,34 +1,34 @@
-package main
+// package main
 
-import (
-	"fmt"
-	// "math"
-)
+// import (
+// 	"fmt"
+// 	// "math"
+// )
 
 
-func maxOfThree(a,b,c int)int{
+// func maxOfThree(a,b,c int)int{
 
-var max int
-if a>b{
-	max=a
-}else{
-	max=b
-}
+// var max int
+// if a>b{
+// 	max=a
+// }else{
+// 	max=b
+// }
 
-if c>max{
-	return c
-}
-return max 
+// if c>max{
+// 	// return c
+// }
+// return max 
 
 // maxAB:=math.Max(float64(a),float64(b))   //math.Max(float64()) эту библиотеку можно использовать только с типом float64
 // maxABC:=math.Max(maxAB,float64(c))
 // return int(maxABC)   //это приведение типа float64 в int, так как main ждет int
 
-}
 
 
-func main(){
 
-	fmt.Println(maxOfThree(3,7,5))
-	fmt.Println(maxOfThree(10,2,8))
-}
+// func main(){
+
+// 	fmt.Println(maxOfThree(3,7,5))
+// 	fmt.Println(maxOfThree(10,2,8))
+// }
