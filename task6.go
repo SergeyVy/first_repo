9710177a9c911630ab7c7263b5e3dc87d270f773
Sergey Vydumkin <sergvydum@gmail.com>
2025-06-28 21:33:19 +0300

    task6

--- task6.go
+++ task6.go
@@ -1,29 +1,29 @@
-package main
+// package main
 
-import(
-	"fmt"
-)
+// import(
+// 	"fmt"
+// )
 
-func charCase(r rune)string{
-	switch{
-	case r>='A' && r<='Z':
-	return"Заглавая латинская буква"
-    case r>='z' && r<='z':
-	return"Не заглавная латинская буква"
-	case r>='А' && r<='Я':
-	return "Заглавная кирилическая буква"
-	case r>='а' && r<='я':
-	return "Заглавная кирилическая буква"
-	default:
-		return "Другое"
-}
+// func charCase(r rune)string{
+// 	switch{
+// 	case r>='A' && r<='Z':
+//     return"Заглавая латинская буква"
+//     case r>='z' && r<='z':
+// 	return"Не заглавная латинская буква"
+// 	case r>='А' && r<='Я':
+// 	return "Заглавная кирилическая буква"
+// 	case r>='а' && r<='я':
+// 	return "Заглавная кирилическая буква"
+// 	default:
+// 		return "Другое"
+// }
 
-}
+// }
 
-func main(){
+// func main(){
 
-	fmt.Println(charCase('A'))
-	fmt.Println(charCase('я'))
-	fmt.Println(charCase('Б'))
-	fmt.Println(charCase('#'))
-}
+// 	fmt.Println(charCase('A'))
+// 	fmt.Println(charCase('я'))
+// 	fmt.Println(charCase('Б'))
+// 	fmt.Println(charCase('#'))
+// }
