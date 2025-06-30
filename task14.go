fe0fa6450629f2e2276984f6d1f2b8809768bb56
Sergey Vydumkin <sergvydum@gmail.com>
2025-06-30 19:32:40 +0300

    задачка 15 удаление из динамического массива через функцию append

---
+++ task14.go
@@ -0,0 +1,16 @@
+// package main
+
+// import(
+// 	"fmt"
+// )
+
+// func appendValue(slice*[]int,value int){
+
+// 	*slice= append(*slice, value)
+// }
+
+// func main(){
+// 	nums:=[]int{1,2,3}
+// 	appendValue(&nums,4)
+// 	fmt.Println(nums)
+// }
