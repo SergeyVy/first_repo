e8cc0bce9bb5e1a83fee9f0d015f0d7f883af9bb
Sergey Vydumkin <sergvydum@gmail.com>
2025-06-30 18:10:23 +0300

    task12 указатели

--- task11.go
+++ task11.go
@@ -1,24 +1,21 @@
-package main
-import(
-	"fmt"
-	"math"
-)
+// package main
 
+// import (
+// 	"fmt"
+// 	"math"
+// )
 
-func powerFunc(exp int) func(int)int{
+// func powerFunc(exp int)func(x int)int{
+// 	return func(x int)int{
+// 		return int(math.Pow(float64(x),float64(exp)))
+// 	}
+// }
 
-	return func(x int)int{
-		return int(math.Pow(float64(x),float64(exp)))
-	}
-}
+// func main(){
 
-func main(){
+// 	square:=powerFunc(3)
+// 	fmt.Println(square(4))
 
-
-	square := powerFunc(2)
-	fmt.Println(square(4))
-
-	krug := powerFunc(3)
-	fmt.Println(krug(4))
-
-}
+// 	krug:=powerFunc(3)
+// 	fmt.Println(krug(6))
+// }
